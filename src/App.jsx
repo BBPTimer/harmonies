b779accd8b4c1d748f20253d6f21999a921d9f7d
BBPTimer <greg.weseloh@gmail.com>
2026-01-14 16:21:30 -0600

    Added reload button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Icon, IconButton } from "@mui/material";
 import "./App.css";
 import Scoresheet from "./components/Scoresheet";
 
 function App() {
   return (
-    <div className="flex">
-      <Scoresheet name={"Heidi"} />
-      <Scoresheet name={"Greg"} />
-    </div>
+    <>
+      <div className="flex">
+        <Scoresheet name={"Heidi"} />
+        <Scoresheet name={"Greg"} />
+      </div>
+      <div className="flex">
+        <IconButton>
+          <Icon sx={{ color: "darkgray" }} onClick={() => window.location.reload()}>
+            refresh
+          </Icon>
+        </IconButton>
+      </div>
+    </>
   );
 }
 
